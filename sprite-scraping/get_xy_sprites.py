b73924a2aa4a01e6e35d51fc2e411c45836248f8
mohsaad <msaad96@gmail.com>
2015-08-15 02:36:45 -0400

    finished scraper, added timer to prevent throttling

--- sprite-scraping/get_xy_sprites.py
+++ sprite-scraping/get_xy_sprites.py
@@ -7,6 +7,7 @@
 import requests
 import os
 import shutil
+import time
 
 class XYScraper:
 
@@ -15,7 +16,7 @@ class XYScraper:
 
 		self.url = 'http://serebii.net/xy/pokemon/'
 		self.curr_entry = 1
-		self.path = '/home/saad/Documents/Github/PokeVision/xy-sprites' # set to your scraping directory
+		self.path = '/home/saad/Documents/Github/PokeVision/xy-sprites/' # set to your scraping directory
 
 	# grab an individual sprite
 	def grab_sprite(self, number):
@@ -26,6 +27,7 @@ class XYScraper:
 		if r.status_code == 200:
 			path = self.path + number
 			try:
+				# taken from http://stackoverflow.com/questions/13137817/how-to-download-image-using-requests
 				with open(path, 'wb') as f:
 					for chunk in r:
 						f.write(chunk)
@@ -33,19 +35,41 @@ class XYScraper:
 				print "Write failed"
 				return False
 		else:
+			# just a bunch of error handling conditions
 			print "Scrape failed"
 			print "Status code: {0}".format(str(r.status_code))
 			return False
 
+		# print which sprite we grabbed
 		print "Sprite #{0} grabbed.".format(number) 
 		return True
 
+	# now loop over and grab a bunch of them
+	def grab_num_sprites(self, num_sprites):
+		while self.curr_entry <= num_sprites:
+			num = str(self.curr_entry)
+			# make sure each number is at least 3 digits
+			# if not, append zeros to the front
+			while len(num) < 3:
+				num = '0' + num # append zeros to the front
 
-	
+
+			res = self.grab_sprite(num)
+			if not res:
+				break # break out of loop if something happens (too many requests, etc.)
+			
+			self.curr_entry += 1 # iterate
+			time.sleep(5) # sleep to not overload server
+
+		print "All {0} sprites grabbed!".format(str(num_sprites))
+
+
+
+
 
 def main():
 	xy = XYScraper()
-	xy.grab_sprite('001')
+	xy.grab_num_sprites(151)
 
 if __name__ == '__main__':
 	main()
